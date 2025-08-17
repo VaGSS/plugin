1b92e2f6e627b461daeb0f1f68bb58215777a62b
VaGS <87600050+VaGSS@users.noreply.github.com>
2025-08-17 14:46:13 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,6 @@ rootProject.name = "DiscordWhitelistPlugin"
 		repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
 		repositories {
 			mavenCentral()
-			maven { url 'https://jitpack.io' }
+			maven { url = uri("https://jitpack.io") }
 		}
 	}
